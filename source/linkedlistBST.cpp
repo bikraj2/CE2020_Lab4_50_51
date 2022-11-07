428cc7e0a8ce6d68e11dc13af8a1d979cf319f29
sitjan <sitaulajan@gmail.com>
2022-11-07 15:22:14 +0545

    Added

--- source/linkedlistBST.cpp
+++ source/linkedlistBST.cpp
@@ -2,5 +2,22 @@
 #include "linkedlistBST.h"
 using namespace std;
 
+BST_linkedlist ::BST_linkedlist (){ root = NULL;}; // calling constructor 
 
+//function to determine if the tree is empty or not//
+bool  BST_linkedlist ::isEmpty (){
+    if (root == NULL)
+    {
+        return true;
+    }
+}
+
+void BST_linkedlist ::add(int key, int value){
+        
+        if(isEmpty()==1){
+            
+
+        }
+
+}
 
